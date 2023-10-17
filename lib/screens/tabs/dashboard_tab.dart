1da233278bc0db7c4c9507038ec11529065641ea
Lans12345 <olanalans12345@gmail.com>
2023-10-18 07:58:23 +0800

    added dashboard ata

--- lib/screens/tabs/dashboard_tab.dart
+++ lib/screens/tabs/dashboard_tab.dart
@@ -1,9 +1,43 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:sk_admin/widgets/text_widget.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
 
-class DashboardTab extends StatelessWidget {
+class DashboardTab extends StatefulWidget {
   const DashboardTab({super.key});
+
+  @override
+  State<DashboardTab> createState() => _DashboardTabState();
+}
+
+class _DashboardTabState extends State<DashboardTab> {
+  @override
+  void initState() {
+    super.initState();
+    getDataMonth();
+  }
+
+  bool hasLoaded = false;
+
+  List users = [];
+
+  getDataMonth() async {
+    for (int i = 0; i < 10; i++) {
+      var collection = FirebaseFirestore.instance
+          .collection('Users')
+          .where('purok', isEqualTo: 'Purok $i');
+
+      var querySnapshot = await collection.get();
+
+      // datasMonth.add(querySnapshot.size);
+
+      users.add(querySnapshot.size);
+    }
+
+    setState(() {
+      hasLoaded = true;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -226,14 +260,42 @@ class DashboardTab extends StatelessWidget {
             const SizedBox(
               height: 20,
             ),
-            Container(
-              color: Colors.grey,
-              width: 650,
-              height: 300,
-            ),
+            hasLoaded
+                ? SizedBox(
+                    width: 650,
+                    height: 300,
+                    child: SfCartesianChart(
+
+                        // Initialize category axis
+                        primaryXAxis: CategoryAxis(),
+                        series: <ChartSeries>[
+                          // Initialize line series
+                          LineSeries<ChartData1, String>(
+                              dataSource: [
+                                // Bind data source
+                                for (int i = 0; i < users.length; i++)
+                                  ChartData1('Purok $i', users[i]),
+                              ],
+                              xValueMapper: (ChartData1 data, _) => data.x,
+                              yValueMapper: (ChartData1 data, _) => data.y)
+                        ]),
+                  )
+                : const CircularProgressIndicator()
           ],
         ),
       ),
     );
   }
+}
+
+class ChartData {
+  ChartData(this.x, this.y);
+  final String x;
+  final double y;
+}
+
+class ChartData1 {
+  ChartData1(this.x, this.y);
+  final String x;
+  final double? y;
 }
